1392d37d4e76bf1d72d4858affaee4795ddb5820
Rasim <eferiums@yandex.ru>
2025-02-25 01:55:55 +0300

    first commit

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, nihil. Cumque, quia obcaecati temporibus quis impedit sunt quo numquam porro eius placeat veritatis culpa recusandae tempora, accusantium saepe soluta atque.</div>
+    </div>
+  );
+}
+
+export default Home
