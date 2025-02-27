1b69c00d082e54398f0e52149cf267b08aff0fb0
Rasim <eferiums@yandex.ru>
2025-02-27 15:14:47 +0300

    feat(admin): Добавление функционала на изменение роли и удаления пользователя

--- src/features/admin/api/api.ts
+++ src/features/admin/api/api.ts
@@ -16,3 +16,23 @@ export const getAlUser = async ({ page, limit }: { page: number; limit: number }
     console.log("Ошибка в получении пользователей", error);
   }
 };
+
+export const updateRole = async (id: string | undefined, data: string) => {
+  try {
+    const res = await axios.put(`http://localhost:1200/api/admin/user/update/${id}`, {
+      role: data
+    })
+    return res.data
+  } catch (error) {
+    console.log('Ошибка при изменении роли', error);
+  }
+}
+
+export const deleteUser = async (id: string | undefined) => {
+  try {
+    const res = await axios.delete(`http://localhost:1200/api/admin/user/${id}`)
+    return res.data
+  } catch (error) {
+    console.log('Ошибка при удалении пользователя', error);
+  }
+}
--- src/features/admin/ui/table.tsx
+++ src/features/admin/ui/table.tsx
@@ -1,34 +1,48 @@
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import { IDashboardResponse } from "../types/type"
+import React, { useState } from "react"
+import { IUser } from "@/entities/auth/types/type"
+import ModalTable from "./modalTable"
 
 interface DashboardTableProps {
   users: IDashboardResponse | null
 }
 
 const DashboardTable: React.FC<DashboardTableProps> = ({ users }) => {
+  const [openDialog, setOpenDialog] = useState(false)
+  const [userData, setUserData] = useState<IUser | null>(null)
+
+  const handleOpen = (user: IUser) => {
+    setOpenDialog(true)
+    setUserData(user)
+  }
+
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>id</TableHead>
-          <TableHead>Имя</TableHead>
-          <TableHead>Фамилия</TableHead>
-          <TableHead>Почта</TableHead>
-          <TableHead>Роль</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {Array.isArray(users?.data) && users.data.map((user) => (
-          <TableRow key={user.id}>
-            <TableCell>{user.id}</TableCell>
-            <TableCell>{user.name}</TableCell>
-            <TableCell>{user.surname}</TableCell>
-            <TableCell>{user.email}</TableCell>
-            <TableCell>{user.role}</TableCell>
+    <>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>id</TableHead>
+            <TableHead>Имя</TableHead>
+            <TableHead>Фамилия</TableHead>
+            <TableHead>Почта</TableHead>
+            <TableHead>Роль</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {Array.isArray(users?.data) && users.data.map((user) => (
+            <TableRow className="cursor-pointer" key={user.id} onClick={() => handleOpen(user)}>
+              <TableCell>{user.id}</TableCell>
+              <TableCell>{user.name}</TableCell>
+              <TableCell>{user.surname}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell>{user.role}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <ModalTable openDialog={openDialog} setOpenDialog={setOpenDialog} userData={userData} />
+    </>
   )
 }
 
