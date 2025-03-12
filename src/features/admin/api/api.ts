24cbb37f515385186bd7ea44fde241137796b06f
Rasim <eferiums@yandex.ru>
2025-03-12 18:45:48 +0300

    Update API URL to production backend

--- src/features/admin/api/api.ts
+++ src/features/admin/api/api.ts
@@ -1,9 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
+import { API_URL } from '@/shared/utils/env.url'
+
 export const getAlUser = async ({ page, limit }: { page: number; limit: number }) => {
   try {
-    const res = await axios.get('http://localhost:1200/api/admin/users', {
+    const res = await axios.get(`${API_URL}/api/admin/users`, {
       params: {
         page,
         limit,
@@ -17,7 +19,7 @@ export const getAlUser = async ({ page, limit }: { page: number; limit: number }
 
 export const updateRole = async (id: string | undefined, data: string) => {
   try {
-    const res = await axios.put(`http://localhost:1200/api/admin/user/update/${id}`, {
+    const res = await axios.put(`${API_URL}/api/admin/user/update/${id}`, {
       role: data,
     })
     return res.data
@@ -28,7 +30,7 @@ export const updateRole = async (id: string | undefined, data: string) => {
 
 export const deleteUser = async (id: string | undefined) => {
   try {
-    const res = await axios.delete(`http://localhost:1200/api/admin/user/${id}`)
+    const res = await axios.delete(`${API_URL}/api/admin/user/${id}`)
     return res.data
   } catch (error) {
     console.error('Ошибка при удалении пользователя', error)
@@ -37,7 +39,7 @@ export const deleteUser = async (id: string | undefined) => {
 
 export const getAllCategories = async () => {
   try {
-    const res = await axios.get('http://localhost:1200/api/user/categories')
+    const res = await axios.get(`${API_URL}/api/user/categories`)
     return res.data
   } catch (error) {
     console.error('Ошибка при получении категории', error)
@@ -46,7 +48,7 @@ export const getAllCategories = async () => {
 
 export const createCategoryThunk = createAsyncThunk('createCategory', async (name: string) => {
   try {
-    const res = await axios.post('http://localhost:1200/api/admin/create/category', {
+    const res = await axios.post(`${API_URL}/api/admin/create/category`, {
       name,
     })
     return res.data
@@ -57,7 +59,7 @@ export const createCategoryThunk = createAsyncThunk('createCategory', async (nam
 
 export const createProductThunk = createAsyncThunk('createProduct', async (data: FormData) => {
   try {
-    const res = await axios.post('http://localhost:1200/api/admin/create/product', data, {
+    const res = await axios.post(`${API_URL}/api/admin/create/product`, data, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
